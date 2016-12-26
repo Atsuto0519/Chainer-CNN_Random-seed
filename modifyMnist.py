c06580c2af330461073d63feaaf40c3f12a804d6
Astuto0519 <atsuto.fps@gmail.com>
2016-12-26 15:41:13 +0900

    mnistを一個表示

---
+++ modifyMnist.py
@@ -0,0 +1,38 @@
+import chainer
+from chainer import training
+
+train, test = chainer.datasets.get_mnist()
+
+print(len(train))
+print(len(train[0]))
+print("train[0][0] =", train[0][0])
+print("train[0][1] =", train[0][1])
+print()
+
+for i in range(10):
+    print("train[%d][0] = %d" % (i, int(train[i][1])))
+print()
+
+# ここまでで，データのアクセスが理解できるはず．
+# ここからはデータの表示
+import matplotlib.pyplot as plt
+
+# 1個表示する関数
+def draw_image_onlyone(data):
+    dimX = 28
+    dimY = 28
+    Z = data.reshape(dimY,dimX)   # convert from vector to 28x28 matrix
+    Z = Z[::-1,:]                 # flip vertical
+    plt.xlim(0,dimX)
+    plt.ylim(0,dimY)
+    plt.pcolor(Z)
+    plt.gray()
+    plt.tick_params(labelbottom="off")
+    plt.tick_params(labelleft="off")
+
+# 1個表示
+index = 7
+print("label=%d" % (train[index][1]))
+draw_image_onlyone(train[index][0])
+plt.show()
+
