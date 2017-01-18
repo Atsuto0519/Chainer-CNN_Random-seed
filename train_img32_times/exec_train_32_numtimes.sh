3b066ba32b4d0519858df68df5e4699de3de61b6
Astuto0519 <atsuto.fps@gmail.com>
2017-01-18 14:55:51 +0900

    二つのグループで試行可能

--- train_img32_times/exec_train_32_numtimes.sh
+++ train_img32_times/exec_train_32_numtimes.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# This script needs "train_img32.py" from https://github.com/Atsuto0519/Chainer-CNN_Random-seed
+# This script needs "train_img32.py" from https://github.com/Atsuto0519/Chainer-CNN_Random-seed.
+# Please this file in the directory has groupA and groupB.
+# Directorys of groupA and groupB have train_img32.py(stable version) and train/ and test/.
 
-start_seed=0
 mkdir results
 for i in `seq 1 $1`
 do
     echo $i times:
-    python ./groupA/train_img32_numtimes.py -b 750 -e 10 -g 0 -o "./groupA/results/result_$i" -s `expr $i + $start_seed`
-    python ./groupB/train_img32_numtimes.py -b 750 -e 10 -g 0 -o "./groupB/results/result_$i" -s `expr $i + $start_seed`
-    echo
+    python ./groupA/traim_img32.py -b 750 -e 10 -g 0 -o './groupA/result_$i'
+    python ./groupA/traim_img32.py -b 750 -e 10 -g 0 -o './groupB/result_$i'
 done
